36a74e69d9940bf4ce0edf54297a351a4e9fad51
tiagonovaes <tiagonovaesangelo@gmail.com>
2016-03-07 16:26:50 -0300

    Data atualizada para portugues e horário do Brasil

--- agoravirtual/settings.py
+++ agoravirtual/settings.py
@@ -115,17 +115,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
